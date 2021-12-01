ed553d769855bd19951b560a23e3ccce4b138488
Pjottos <pjotrslooff@icloud.com>
2021-12-01 08:44:01 +0100

    day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,1 +1,49 @@
-fn main() {}
+use std::{
+    collections::VecDeque,
+    io::{self, prelude::*},
+};
+
+fn main() {
+    part_2();
+}
+
+fn part_1() {
+    let res = io::stdin()
+        .lock()
+        .lines()
+        .map(|res| res.unwrap().parse().unwrap())
+        .fold((0, None), |(mut count, prev), v: u32| {
+            if prev.map(|p| v > p).unwrap_or(false) {
+                count += 1;
+            }
+            (count, Some(v))
+        });
+
+    println!("{}", res.0)
+}
+
+fn part_2() {
+    let mut buf = VecDeque::new();
+    let count = io::stdin()
+        .lock()
+        .lines()
+        .map(|res| res.unwrap().parse().unwrap())
+        .fold(0, |mut count, v: u32| {
+            buf.push_back(v);
+
+            if buf.len() == 4 {
+                let a: u32 = buf.iter().take(3).sum();
+                let b: u32 = buf.iter().skip(1).sum();
+
+                if b > a {
+                    count += 1;
+                }
+
+                buf.pop_front();
+            }
+
+            count
+        });
+
+    println!("{}", count)
+}
