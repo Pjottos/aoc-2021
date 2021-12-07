8685d555c0cc3c6132ef6e606fdc6fa421e90d2b
Pjottos <pjotrslooff@icloud.com>
2021-12-07 17:52:32 +0100

    optimize day 6

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -28,18 +28,8 @@ fn main() {
 }
 
 fn run_days(mut bins: [u64; 9], days: usize) -> i64 {
-    for _ in 0..days {
-        // amount of new born fish this day, same number as the amount of parents
-        let new_born = bins[0];
-        // decrease the timer on all fish, parents of this day are overwritten which is why we saved
-        // the amount of parents.
-        for i in 0..bins.len() - 1 {
-            bins[i] = bins[i + 1];
-        }
-        // give birth to the fish, overwriting because the fish that were in this bin are now in 7.
-        bins[8] = new_born;
-        // reset the timer on the parents.
-        bins[6] += new_born;
+    for day in 0..days {
+        bins[(day + 7) % 9] += bins[day % 9];
     }
 
     bins.iter().sum::<u64>() as i64
