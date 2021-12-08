e0f48df70e540a1055f3102519acecf2384b7623
Pjottos <pjotrslooff@icloud.com>
2021-12-08 16:05:02 +0100

    add benchmarking to harness

--- src/harness.rs
+++ src/harness.rs
@@ -11,19 +11,28 @@ pub struct Harness<E> {
     day: Option<u32>,
     input: Option<(E, Duration)>,
     text: Option<String>,
+    bench: bool,
 }
 
 impl<'a, E> Harness<E> {
+    const ITERATIONS: usize = 1 << 15;
+
     pub fn begin() -> Self {
         Self {
             day: None,
             input: None,
             text: None,
+            bench: false,
         }
     }
 
     pub fn day(&'a mut self, day: u32) -> &'a mut Self {
         self.day = Some(day);
+        self
+    }
+
+    pub fn bench(&'a mut self) -> &'a mut Self {
+        self.bench = true;
         self
     }
 
@@ -34,7 +43,7 @@ impl<'a, E> Harness<E> {
 
     pub fn extract<X>(&'a mut self, extractor: X) -> &'a Self
     where
-        X: FnOnce(&'a str) -> E,
+        X: Fn(&'a str) -> E,
     {
         let day = self.day.unwrap();
         if self.text.is_none() {
@@ -48,9 +57,18 @@ impl<'a, E> Harness<E> {
         }
         let text = self.text.as_ref().unwrap();
 
-        let begin = Instant::now();
+        let mut time_sum = 0;
         let input = extractor(text);
-        let extract_time = begin.elapsed();
+
+        if self.bench {
+            for _ in 0..Self::ITERATIONS {
+                let begin = Instant::now();
+                extractor(text);
+                time_sum += begin.elapsed().as_nanos();
+            }
+        }
+
+        let extract_time = Duration::from_nanos((time_sum / Self::ITERATIONS as u128) as u64);
 
         self.input = Some((input, extract_time));
         self
@@ -58,22 +76,31 @@ impl<'a, E> Harness<E> {
 
     pub fn run_part<F, R>(&'a self, part_num: u32, func: F) -> &'a Self
     where
-        F: FnOnce(&E) -> R,
+        F: Fn(&E) -> R,
         R: Debug,
     {
         let (input, extract_time) = self.input.as_ref().expect("input not extracted yet");
 
-        let begin = Instant::now();
+        let mut time_sum = 0;
         let res = func(input);
-        let run_time = begin.elapsed();
+        println!("Part {}: {:?}", part_num, res);
 
-        println!(
-            "part {}: {:?} in {:?} ({:?} excluding extract)",
-            part_num,
-            res,
-            *extract_time + run_time,
-            run_time
-        );
+        if self.bench {
+            println!("Running benchmark...");
+            for _ in 0..Self::ITERATIONS {
+                let begin = Instant::now();
+                func(input);
+                time_sum += begin.elapsed().as_nanos();
+            }
+
+            let run_time = Duration::from_nanos((time_sum / Self::ITERATIONS as u128) as u64);
+
+            println!(
+                "Result: {:?} ({:?} excluding extract)",
+                *extract_time + run_time,
+                run_time,
+            );
+        }
 
         self
     }
