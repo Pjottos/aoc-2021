af463c57b743d898f73cdb25fac547fc8b739443
Pjottos <pjotrslooff@icloud.com>
2021-12-08 21:14:56 +0100

    optimize day 3, part 1 ~43x faster, part 2 ~6x faster

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -5,22 +5,37 @@ const BIT_COUNT: usize = 12;
 fn main() {
     Harness::begin()
         .day(3)
-        .extract(ex::binary_nums)
         // .input_override("00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n")
-        .run_part(1, |nums| {
-            let gamma = make_common_mask(nums);
+        .extract(|text| {
+            text.as_bytes()
+                .chunks_exact(BIT_COUNT + 1)
+                .map(|b| &b[..BIT_COUNT])
+        })
+        .run_part(1, |lines| {
+            let mut counts = [0; BIT_COUNT];
+            let mut line_count = 0;
+            for line in lines.clone() {
+                line.iter()
+                    .zip(counts.iter_mut())
+                    .for_each(|(v, c)| *c += (v - b'0') as u32);
+                line_count += 1;
+            }
+
+            let mut gamma = 0;
+            for (i, count) in counts.iter().rev().copied().enumerate() {
+                let bit = (count * 2 >= line_count) as u32;
+                gamma |= bit << i;
+            }
             let epsilon = !gamma & !(!0 << BIT_COUNT);
 
             // println!("g: {:12.b}\ne: {:12.b}", gamma, epsilon);
 
             gamma * epsilon
         })
-        .run_part(2, |nums| {
-            let mut oxygen = nums.clone();
-            let mut co2 = nums.clone();
+        .run_part(2, |lines| {
+            let mut oxygen: Vec<_> = lines.clone().collect();
+            let mut co2 = oxygen.clone();
             for i in 0..BIT_COUNT {
-                let mask = 1 << (BIT_COUNT - 1 - i);
-
                 // println!(
                 //     "m: {:12.b}, o: {:4}, c: {:4}",
                 //     mask,
@@ -28,44 +43,41 @@ fn main() {
                 //     co2.len()
                 // );
                 if oxygen.len() > 1 {
-                    let common_mask = make_single_common_mask(&oxygen, i);
-                    oxygen.retain(|n| n & mask == common_mask);
+                    retain_common_bit_lines(&mut oxygen, i, false);
                 }
                 if co2.len() > 1 {
-                    let common_mask = make_single_common_mask(&co2, i);
-                    co2.retain(|n| !n & mask == common_mask);
+                    retain_common_bit_lines(&mut co2, i, true);
                 }
             }
 
-            oxygen[0] * co2[0]
+            binary_to_num(oxygen[0]) * binary_to_num(co2[0])
         });
 }
 
-fn make_common_mask(nums: &[u64]) -> u64 {
-    let mut counts = [0; BIT_COUNT];
-    for num in nums {
-        for (i, c) in counts.iter_mut().enumerate() {
-            *c += (num >> i) & 1;
-        }
-    }
-    // println!("{:?}", counts);
-
-    let mut common_mask = 0;
-    for (i, count) in counts.iter().copied().enumerate() {
-        let bit = (count * 2 >= nums.len() as u64) as u64;
-        common_mask |= bit << i;
-    }
-
-    common_mask
-}
-
-fn make_single_common_mask(nums: &[u64], pos: usize) -> u64 {
+fn retain_common_bit_lines(lines: &mut Vec<&[u8]>, bit_idx: usize, invert: bool) {
     // nums.iter().for_each(|n| println!("o: {:12.b}", n));
     // println!();
 
-    let shift = BIT_COUNT - 1 - pos;
-    let count: u64 = nums.iter().map(|n| (n >> shift) & 1).sum();
+    let count: u32 = lines.iter().map(|l| (l[bit_idx] - b'0') as u32).sum();
+    let allowed_value = (b'0' + (count * 2 >= lines.len() as u32) as u8) ^ invert as u8;
 
-    let bit = (count * 2 >= nums.len() as u64) as u64;
-    bit << shift
+    let mut i = 0;
+    while i < lines.len() {
+        if lines[i][bit_idx] == allowed_value {
+            i += 1;
+        } else {
+            lines.swap_remove(i);
+        }
+    }
+}
+
+fn binary_to_num(binary: &[u8]) -> u32 {
+    let mut result = 0;
+
+    for (i, c) in binary.iter().rev().copied().enumerate() {
+        let bit = (c - b'0') as u32;
+        result |= bit << i;
+    }
+
+    result
 }
--- src/ex.rs
+++ src/ex.rs
@@ -1,9 +1,3 @@
 pub fn nums(text: &str) -> Vec<i64> {
     text.lines().map(|l| l.parse().unwrap()).collect()
 }
-
-pub fn binary_nums(text: &str) -> Vec<u64> {
-    text.lines()
-        .map(|l| u64::from_str_radix(l, 2).unwrap())
-        .collect()
-}
