30389eb1a8e81407fe4b0ca76f80143997035be0
Pjottos <pjotrslooff@icloud.com>
2021-12-08 15:20:34 +0100

    immediately parse digits to bits in day 8

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -12,12 +12,13 @@ fn main() {
                     .next()
                     .unwrap()
                     .split_ascii_whitespace()
-                    .map(|w| w.as_bytes());
+                    .map(|w| digit_bits(w.as_bytes()));
                 let output = parts
                     .next()
                     .unwrap()
                     .split_ascii_whitespace()
-                    .map(|w| w.as_bytes());
+                    .map(|w| digit_bits(w.as_bytes()));
+
                 (combos, output)
             })
         })
@@ -26,7 +27,7 @@ fn main() {
                 .clone()
                 .map(|(_, output)| {
                     output
-                        .filter(|w| unique.iter().position(|(l, _)| *l == w.len()).is_some())
+                        .filter(|w| unique.iter().any(|(l, _)| *l as u32 == w.count_ones()))
                         .count() as u32
                 })
                 .sum::<u32>()
@@ -35,50 +36,45 @@ fn main() {
             lines
                 .clone()
                 .map(|(combos, encoded)| {
-                    let mut digits = [0u8; 10];
+                    let mut digits = [0; 10];
 
                     for c in combos.clone() {
-                        if let Some((_, v)) = unique.iter().find(|(l, _)| *l == c.len()) {
-                            let bits = digit_bits(c);
-                            digits[*v] = bits;
+                        if let Some((_, v)) =
+                            unique.iter().find(|(l, _)| *l as u32 == c.count_ones())
+                        {
+                            digits[*v] = c;
                         }
                     }
 
                     // all unique digits should have been found
                     assert!(unique.iter().all(|(_, v)| digits[*v] != 0));
 
-                    for r in combos
-                        .map(|c| digit_bits(&c))
-                        .filter(|&bits| !unique.iter().any(|(l, _)| *l as u32 == bits.count_ones()))
+                    for r in
+                        combos.filter(|&c| !unique.iter().any(|(l, _)| *l as u32 == c.count_ones()))
                     {
                         let digit = if r.count_ones() == 5 {
                             if r & digits[1] == digits[1] {
                                 3
+                            } else if (r & digits[4]).count_ones() == 2 {
+                                2
                             } else {
-                                if (r & digits[4]).count_ones() == 2 {
-                                    2
-                                } else {
-                                    5
-                                }
+                                5
+                            }
+                        } else if r & digits[1] == digits[1] {
+                            if r & digits[4] == digits[4] {
+                                9
+                            } else {
+                                0
                             }
                         } else {
-                            if r & digits[1] == digits[1] {
-                                if r & digits[4] == digits[4] {
-                                    9
-                                } else {
-                                    0
-                                }
-                            } else {
-                                6
-                            }
+                            6
                         };
 
                         digits[digit] = r;
                     }
 
                     encoded.enumerate().fold(0, |num, (i, o)| {
-                        let bits = digit_bits(o);
-                        let digit = digits.iter().position(|&b| b == bits).unwrap() as u32;
+                        let digit = digits.iter().position(|&b| b == o).unwrap() as u32;
 
                         num + digit * 10u32.pow(3 - i as u32)
                     })
@@ -89,7 +85,7 @@ fn main() {
 
 fn digit_bits(chars: &[u8]) -> u8 {
     chars.iter().fold(0, |result, &c| {
-        let bit = 1 << (c - 'a' as u8);
+        let bit = 1 << (c - b'a');
         result | bit
     })
 }
