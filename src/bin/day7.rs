8a6131e03ab8e750fce553ccbf93ade4c8cc9168
Pjottos <pjotrslooff@icloud.com>
2021-12-10 09:19:08 +0100

    comment day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -4,30 +4,45 @@ fn main() {
     Harness::begin()
         .day(7)
         .extract(|text| {
+            // Parse the input as a list of numbers.
             let mut result: Vec<i32> = text.split(',').map(|p| p.trim().parse().unwrap()).collect();
+
+            // Both parts require the input to be sorted, besides, we don't have mutable
+            // access in the part closures so we must sort it here.
             result.sort_unstable();
+
             result
         })
         .run_part(1, |positions| {
-            // median
+            // The value in an array with the minimum sum of absolute differences to
+            // the other values of the array is the median.
+            // Since the input is sorted we can simply take the middle element to get
+            // the median position.
             let target_pos = positions[positions.len() / 2];
+
+            // Calculate the total fuel cost.
             positions
                 .iter()
                 .map(|&p| (target_pos - p).abs())
                 .sum::<i32>()
         })
         .run_part(2, |positions| {
+            // Calculates the total amount of fuel spent to move all crabs to
+            // the specified position
             let cost_function = |target_pos: i32| -> i32 {
                 positions
                     .iter()
                     .map(|&pos| {
                         let steps = (target_pos - pos).abs();
-                        // automatically uses (n * (n + 1)) / 2
+                        // Every step takes 1 additional fuel over the cost
+                        // of the last step.
                         (1..=steps).sum::<i32>()
                     })
                     .sum()
             };
 
+            // Perform a ternary search to find the position with
+            // the lowest total fuel cost.
             let mut left = 0;
             let mut right = *positions.last().unwrap();
             let mut min_cost = None;
