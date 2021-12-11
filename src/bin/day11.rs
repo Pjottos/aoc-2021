d49dfb22af0ee07de711b0a07643944a01352cd8
Pjottos <pjotrslooff@icloud.com>
2021-12-11 14:35:40 +0100

    optimize day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -2,22 +2,35 @@ use aoc_2021::*;
 
 const GRID_DIM: usize = 10;
 
+// Add padding on all 4 sides of the grid
+const DIM_SIZE: usize = GRID_DIM + 2;
+const GRID_SIZE: usize = DIM_SIZE * DIM_SIZE;
+
 fn main() {
     Harness::begin()
         .day(11)
         // .input_override("5483143223\n2745854711\n5264556173\n6141336146\n6357385478\n4167524645\n2176841721\n6882881134\n4846848554\n5283751526\n")
         .extract(|text| {
-            text.as_bytes()
+            let mut bytes = text
+                .as_bytes()
                 .iter()
                 .enumerate()
                 // Skip newlines
-                .filter_map(|(i, &c)| (i % (GRID_DIM + 1) < GRID_DIM).then(|| c - b'0'))
-        })
-        .run_part(1, |bytes| {
-            let mut grid: Vec<_> = bytes.clone().collect();
-            let mut flashes = 0;
+                .filter_map(|(i, &c)| (i % (GRID_DIM + 1) < GRID_DIM).then(|| c - b'0'));
 
-            assert_eq!(grid.len(), GRID_DIM * GRID_DIM);
+            let mut grid = [0; GRID_SIZE];
+            for y in 0..GRID_DIM {
+                for x in 0..GRID_DIM {
+                    let i = grid_index(x, y);
+                    grid[i] = bytes.next().unwrap();
+                }
+            }
+
+            grid
+        })
+        .run_part(1, |grid| {
+            let mut grid = *grid;
+            let mut flashes = 0;
 
             for _step in 0..100 {
                 flashes += step_grid(&mut grid);
@@ -25,13 +38,11 @@ fn main() {
 
             flashes
         })
-        .run_part(2, |bytes| {
-            let mut grid: Vec<_> = bytes.clone().collect();
-
-            assert_eq!(grid.len(), GRID_DIM * GRID_DIM);
+        .run_part(2, |grid| {
+            let mut grid = *grid;
 
             for step in 0..usize::MAX {
-                if step_grid(&mut grid) == grid.len() as u32 {
+                if step_grid(&mut grid) == (GRID_DIM * GRID_DIM) as u32 {
                     return step + 1;
                 }
             }
@@ -40,86 +51,34 @@ fn main() {
         });
 }
 
-fn step_grid(grid: &mut [u8]) -> u32 {
+fn step_grid(grid: &mut [u8; GRID_SIZE]) -> u32 {
     let mut flashes = 0;
 
     grid.iter_mut().for_each(|c| *c += 1);
 
     loop {
         let mut any = false;
-        for i in 0..grid.len() {
-            if grid[i] <= 9 || grid[i] >= 0x80 {
-                continue;
-            }
+        for y in 0..GRID_DIM {
+            for x in 0..GRID_DIM {
+                let i = grid_index(x, y);
 
-            any = true;
-            flashes += 1;
-            grid[i] = 0x80;
+                if grid[i] > 9 && grid[i] < 0x80 {
+                    any = true;
+                    flashes += 1;
 
-            if i == 0 {
-                // Top left corner
-                grid[1] += 1;
-                grid[GRID_DIM] += 1;
-                grid[GRID_DIM + 1] += 1;
-            } else if i < GRID_DIM - 1 {
-                // Top row
-                grid[i - 1] += 1;
-                grid[i + 1] += 1;
+                    grid[i] |= 0x80;
 
-                grid[i + GRID_DIM - 1] += 1;
-                grid[i + GRID_DIM] += 1;
-                grid[i + GRID_DIM + 1] += 1;
-            } else if i == GRID_DIM - 1 {
-                // Top right corner
-                grid[i - 1] += 1;
-                grid[i + GRID_DIM - 1] += 1;
-                grid[i + GRID_DIM] += 1;
-            } else if i == grid.len() - 1 {
-                // Bottom right corner
-                grid[i - 1] += 1;
-                grid[i - GRID_DIM] += 1;
-                grid[i - GRID_DIM - 1] += 1;
-            } else if i % GRID_DIM == GRID_DIM - 1 {
-                // Right column
-                grid[i - GRID_DIM] += 1;
-                grid[i + GRID_DIM] += 1;
+                    grid[i - DIM_SIZE - 1] += 1;
+                    grid[i - DIM_SIZE] += 1;
+                    grid[i - DIM_SIZE + 1] += 1;
 
-                grid[i - GRID_DIM - 1] += 1;
-                grid[i - 1] += 1;
-                grid[i + GRID_DIM - 1] += 1;
-            } else if i > grid.len() - GRID_DIM {
-                // Bottom row
-                grid[i - 1] += 1;
-                grid[i + 1] += 1;
+                    grid[i - 1] += 1;
+                    grid[i + 1] += 1;
 
-                grid[i - GRID_DIM - 1] += 1;
-                grid[i - GRID_DIM] += 1;
-                grid[i - GRID_DIM + 1] += 1;
-            } else if i == grid.len() - GRID_DIM {
-                // Bottom left corner
-                grid[i + 1] += 1;
-                grid[i - GRID_DIM] += 1;
-                grid[i - GRID_DIM + 1] += 1;
-            } else if i % GRID_DIM == 0 {
-                // Left column
-                grid[i - GRID_DIM] += 1;
-                grid[i + GRID_DIM] += 1;
-
-                grid[i - GRID_DIM + 1] += 1;
-                grid[i + 1] += 1;
-                grid[i + GRID_DIM + 1] += 1;
-            } else {
-                // Any cell not at an edge
-                grid[i - GRID_DIM - 1] += 1;
-                grid[i - GRID_DIM] += 1;
-                grid[i - GRID_DIM + 1] += 1;
-
-                grid[i - 1] += 1;
-                grid[i + 1] += 1;
-
-                grid[i + GRID_DIM - 1] += 1;
-                grid[i + GRID_DIM] += 1;
-                grid[i + GRID_DIM + 1] += 1;
+                    grid[i + DIM_SIZE - 1] += 1;
+                    grid[i + DIM_SIZE] += 1;
+                    grid[i + DIM_SIZE + 1] += 1;
+                }
             }
         }
 
@@ -134,6 +93,11 @@ fn step_grid(grid: &mut [u8]) -> u32 {
         .for_each(|c| *c = 0);
 
     flashes
+}
+
+#[inline]
+fn grid_index(x: usize, y: usize) -> usize {
+    (y + 1) * DIM_SIZE + x + 1
 }
 
 #[allow(dead_code)]
